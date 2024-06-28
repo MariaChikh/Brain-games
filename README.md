754f4dc347bb6fb31c1da6d5827987a71c91b80c
Maria Chikhireva <mchikhireva@gmail.com>
2024-06-28 20:16:28 +0300

    step five

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/MariaChikh/python-project-49/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/MariaChikh/python-project-49/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/a5749b8a1fbd76dcf72f/maintainability)](https://codeclimate.com/github/MariaChikh/python-project-49/maintainability)
+
+Asciinema https://asciinema.org/a/dCjNrCBC75HjFcsI67JkmTGVh
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,35 +1,42 @@
 #!/usr/bin/env python3
 
 from random import randint
-from working_files.welcome import welcome_user
+from brain_games.scripts.welcome import welcome_user
 
-print('Answer "yes" if the number is even, otherwise answer "no".')
+def main():
 
-def is_even(number):
-    if number%2==0:
-        return True
-    else:
-        return False
+    name=welcome_user()
 
-k=0
-while k<3:
-    number=randint(1, 100)
-    print(f'Question: {number}')
-    answer=input('Your answer: ')
+    print('Answer "yes" if the number is even, otherwise answer "no".')
 
-    if is_even(number)==True:
-        correct_answer = 'yes'
-    else:
-        correct_answer = 'no'
+    def is_even(number):
+        if number%2==0:
+            return True
+        else:
+            return False
+
+    k=0
+    while k<3:
+        number=randint(1, 100)
+        print(f'Question: {number}')
+        answer=input('Your answer: ')
+
+        if is_even(number)==True:
+            correct_answer = 'yes'
+        else:
+            correct_answer = 'no'
     
-    if is_even(number) == True and answer == 'yes':
-        print('Correct!')
-        k+=1
-    elif is_even(number) == False and answer == 'no':
-        print('Correct!')
-        k+=1
-    else:
-        print(f'{answer} is wrong answer ;(. Correct answer was {correct_answer})')
-        break
-if k==3:
-    print(f'Congratulations, {name}!')
+        if is_even(number) == True and answer == 'yes':
+            print('Correct!')
+            k+=1
+        elif is_even(number) == False and answer == 'no':
+            print('Correct!')
+            k+=1
+        else:
+            print(f"{answer} is wrong answer ;(. Correct answer was {correct_answer} \nLet's try again, {name}!")
+            break
+    if k==3:
+        print(f'Congratulations, {name}!')
+
+if __name__ == '__main__':
+    main()
