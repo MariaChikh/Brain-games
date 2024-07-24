bdb3b088c495ad2f70c30523b4cff335ef31245e
MariaChikh <mchikhireva@gmail.com>
2024-07-24 22:50:29 +0300

    fix flake8 errors

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -10,8 +10,7 @@ def brain_prime():
     def generate_question_and_answer():
 
         def is_prime(n):
-            if n == 1:
-                return False
+
             for i in range(2, (int(n**0.5) + 1)):
                 if n % i == 0:
                     return False
