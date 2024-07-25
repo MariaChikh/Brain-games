c0c382ffd4857c3d3b82b989742d71eb9da05544
MariaChikh <mchikhireva@gmail.com>
2024-07-25 07:35:29 +0300

    fix linter error in calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -10,7 +10,7 @@ def brain_calc():
     def generate_question_and_answer():
 
         operation = ['+', '-', '*']
-        question = (f'{randint(1, 10)} {choice(operation)} {randint(1 , 10)}')
+        question = (f'{randint(1, 10)} {choice(operation)} {randint(1, 10)}')
         correct_answer = eval(question)
         return question, correct_answer
 
