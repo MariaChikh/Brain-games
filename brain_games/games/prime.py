dd922fd914f4036d28798761991b8ee46a86bd97
MariaChikh <mchikhireva@gmail.com>
2024-07-29 19:45:57 +0300

    fix prime game

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -5,6 +5,9 @@ def generate_question_and_answer():
 
     def is_prime(n):
 
+        if n == 1:
+            return True
+
         for i in range(2, (int(n**0.5) + 1)):
             if n % i == 0:
                 return False
