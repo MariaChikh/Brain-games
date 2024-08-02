c43d2cf1f42096279530ae3a15cc93929b580791
MariaChikh <mchikhireva@gmail.com>
2024-08-02 18:58:08 +0300

    fix calc.py

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -10,11 +10,11 @@ def generate_question_and_answer():
     operation = choice(['+', '-', '*'])
     num_1 = randint(START, STOP)
     num_2 = randint(START, STOP)
+    question = f'{num_1}{operation}{num_2}'
     if operation == '+':
         correct_answer = num_1 + num_2
     elif operation == '-':
         correct_answer = num_1 - num_2
     else:
         correct_answer = num_1 * num_2
-    question = (f'{num_1}{operation}{num_2}')
     return question, correct_answer
